72e16836b96c6470abfae78f8e829d2c376eadb9
Sarapan Malam Games <sarapan.malam.games@gmail.com>
2022-09-15 22:53:10 +0800

    add main-screen-plugin

---
+++ godot4/main-screen-plugin/addons/main_screen/main_panel.gd
@@ -0,0 +1,6 @@
+@tool
+extends CenterContainer
+
+
+func _on_button_pressed():
+	print("Hello from the main screen plugin!")
---
+++ godot4/main-screen-plugin/addons/main_screen/main_screen.gd
@@ -0,0 +1,33 @@
+@tool
+extends EditorPlugin
+
+const MainPanel: PackedScene = preload("res://addons/main_screen/main_panel.tscn")
+
+var main_panel_instance
+
+func _enter_tree():
+	main_panel_instance = MainPanel.instantiate()
+	get_editor_interface().get_editor_main_screen().add_child(main_panel_instance)
+	_make_visible(false)
+
+
+func _exit_tree():
+	if main_panel_instance:
+		main_panel_instance.queue_free()
+
+
+func _has_main_screen():
+	return true
+
+
+func _make_visible(visible):
+	if main_panel_instance:
+		main_panel_instance.visible = visible
+
+
+func _get_plugin_name():
+	return "Main Screen Plugin"
+
+
+func _get_plugin_icon():
+	return get_editor_interface().get_base_control().get_theme_icon("Node", "EditorIcons")
